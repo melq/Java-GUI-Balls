6c7e6e3f8bca233e2163feeaaab9502ab127f31c
melq <tu2sice@yahoo.co.jp>
2021-01-05 15:07:22 +0900

    :sparkles: 右クリックでボールを追加できるようにした

--- src/PinBall.java
+++ src/PinBall.java
@@ -1,6 +1,10 @@
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -22,8 +26,8 @@ public class PinBall {
     }
 }
 
-class Panel4GameBoard extends JPanel implements ActionListener {
-    private final Ball ball;
+class Panel4GameBoard extends JPanel implements ActionListener, MouseListener {
+    private final List<Ball> ballList;
     private final Timer timer;
 
     // 再描画タイミング
@@ -31,23 +35,57 @@ class Panel4GameBoard extends JPanel implements ActionListener {
 
     public Panel4GameBoard() {
         timer = new Timer(INTERVAL, this);
-        ball = new Ball(this);
+        ballList = new ArrayList<Ball>();
+        ballList.add(new Ball(this));
+        addMouseListener(this);
     }
 
     public void animationStart() {
-        ball.goHome();
+        ballList.get(0).goHome();
         timer.start();
     }
 
     @Override
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
-        ball.draw(graphics);
+        for (Ball ball: ballList) {
+            ball.draw(graphics);
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent event) {
-        ball.next();
+        for (Ball ball: ballList) {
+            ball.next();
+        }
         repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON3) {
+            System.out.println("button2 clicked");
+            ballList.add(new Ball(this));
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
